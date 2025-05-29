1eab84b26f2db2fd2d86908ff575242f27d83b60
kumeyuta <ku8.m.1.e2@gmail.com>
2025-05-29 14:23:31 +0900

    wbr追加

--- src/ts/ariaHiddenToBr.ts
+++ src/ts/ariaHiddenToBr.ts
@@ -3,7 +3,7 @@
  */
 
 export const addAriaHiddenToBr = (): void => {
-  const brTags = document.querySelectorAll<HTMLBRElement>('br');
+  const brTags = document.querySelectorAll<HTMLBRElement>('br, wbr');
 
   brTags.forEach((brTag) => {
     if (!brTag.hasAttribute('aria-hidden')) brTag.setAttribute('aria-hidden', 'true');
